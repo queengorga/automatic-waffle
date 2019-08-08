5fdf345cb7418eba581c3af55190e36d5f54cabe
Victoria Allen <vickmae.allen@gmail.com>
2019-08-08 14:08:49 -0700

    Finished lesson 3

---
+++ variables.js
@@ -0,0 +1,13 @@
+// 1. You can't create a variable more than once
+let petName = 'Bea'
+
+petName = 'Buster'
+
+console.log(petName)
+
+// 2. There are rules related to variable names
+let goal_ = 15
+let total2 = 8 + 8
+
+// 3. Variable names cannot be reserved keywords
+// let var = 2
