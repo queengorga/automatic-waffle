bc16b45529a40df4a0ec6936bf7149e7a365e8fa
Victoria Allen <vickmae.allen@gmail.com>
2019-08-09 18:40:20 -0700

    Exercise 3.6 - Booleans

--- boolean.js
+++ boolean.js
@@ -1,7 +1,19 @@
 // === - equality operator
 // !== - not equal operator
+// < - less than operator
+// > - greater than operator
+// >= - greater than or equal to operator
+// <= - less than or equal to operator
 
-let temp = 31
-let isFreezing = temp < 
+// Create age set to your age
+// Calculate is age - if they are 7 or under
+// Calculate is senior - if they are 65 or older
+// Print is child value
+// Print is senior value 
 
-console.log(isFreezing)
+let age = 36
+let child = age <= 7
+let senior = age >= 65
+
+console.log(child)
+console.log(senior)
