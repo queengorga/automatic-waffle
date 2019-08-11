b98a3c5891ccb3094c9821c6db2ab3216f107bfd
Victoria Allen <vickmae.allen@gmail.com>
2019-08-11 14:50:47 -0700

    Lesson 3.8  Exercise

--- boolean-advanced.js
+++ boolean-advanced.js
@@ -3,15 +3,20 @@ let userRole = 'admin'
 
 if (isAccountLocked) {
     console.log('Is account locked')
-} else {
-    console.log('Welcome!')
-}
-
-if (isAccountLocked) {
-    console.log('Is account locked')
 } else if (userRole === 'admin') {
     console.log('Welcome Admin')
 } else {
     console.log('Welcome')
 }
 
+// Challenge
+
+let temp = 4
+
+if (temp <= 32) {
+    console.log('It is freezing outside!')
+} else if (temp >= 110) {
+    console.log('It is hot outside!')
+} else {
+    console.log('Go for it. It is pretty nice out')
+}
