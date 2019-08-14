950cf6a307e40ffbe07fb5bb83b3311c95469650
Victoria Allen <vickmae.allen@gmail.com>
2019-08-13 21:13:00 -0700

    Lesson 3.9 Logical And Or

--- logical-and-or.js
+++ logical-and-or.js
@@ -3,3 +3,9 @@ let temp = 65
 if (temp >= 60 && temp <= 90) {
     console.log('It is pretty nice out.')
 }
+
+if (temp <= 0 || temp >= 120) {
+    console.log('Do not go outside')
+} else {
+    console.log('Do what you want')
+}
