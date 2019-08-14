040c4911a6c75947622f8c082ac95bb76172b9de
Victoria Allen <vickmae.allen@gmail.com>
2019-08-13 21:19:24 -0700

    Lesson 3.9 Exercise

--- logical-and-or.js
+++ logical-and-or.js
@@ -4,8 +4,23 @@ if (temp >= 60 && temp <= 90) {
     console.log('It is pretty nice out.')
 }
 
-if (temp <= 0 || temp >= 120) {
+else if (temp <= 0 || temp >= 120) {
     console.log('Do not go outside')
 } else {
     console.log('Do what you want')
+}
+
+//Challenge Area
+
+let isGuestOneVegan = false
+let isGuestTwoVegan = false
+
+if (isGuestOneVegan && isGuestTwoVegan) {
+    console.log('Only offer vegan.')
+}
+
+else if (isGuestOneVegan || isGuestTwoVegan) {
+    console.log('Make sure to offer vegan and non-vegan menu')
+} else {
+    console.log('Offer anything on the menu')
 }
