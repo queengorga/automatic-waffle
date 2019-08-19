65bae011a55a56a8090b30523351fde5305cc171
Victoria Allen <vickmae.allen@gmail.com>
2019-08-18 18:13:32 -0700

    Lesson 4.10 - Exercise

--- functions101.js
+++ functions101.js
@@ -21,3 +21,13 @@ console.log(otherValue)
 
 //Challenge Area
 
+let convertFahrenheitToCelsius = function (fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+}
+
+let tempOne = convertFahrenheitToCelsius(32)
+let tempTwo = convertFahrenheitToCelsius(68)
+
+console.log(tempOne)
+console.log(tempTwo)
