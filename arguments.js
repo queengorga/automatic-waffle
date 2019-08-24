96145e4bba5cd22e11eed43bca0a51a4da71cddb
Victoria Allen <vickmae.allen@gmail.com>
2019-08-23 19:44:13 -0700

    Lesson 4.4 - Arguments Challenge

--- arguments.js
+++ arguments.js
@@ -11,5 +11,14 @@ let getScoreText = function (name = 'Bea', score = 100) {
     return 'Name: ' + name + ' Score: ' + score
 }
 
-let scoreText = getScoreText()
+let scoreText = getScoreText(undefined , 99)
 console.log(scoreText)
+
+// Challenge 
+
+let getTip = function (total, tipPercent = .2) {
+    return total * tipPercent
+}
+
+let tip = getTip(40, .25)
+console.log(tip)
