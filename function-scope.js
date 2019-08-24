6f1e2de19d521af09321c28a4cf2f64a97bec181
Victoria Allen <vickmae.allen@gmail.com>
2019-08-23 19:46:59 -0700

    Lesson 4.5 - Function Scopes

---
+++ function-scope.js
@@ -0,0 +1,10 @@
+let convertFahrenheitToCelsius = function (fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+}
+
+let tempOne = convertFahrenheitToCelsius(32)
+let tempTwo = convertFahrenheitToCelsius(68)
+
+console.log(tempOne)
+console.log(tempTwo)
