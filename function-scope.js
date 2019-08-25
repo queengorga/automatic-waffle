337b6d43eb59830a2893d9fc72b83c58955ee44c
Victoria Allen <vickmae.allen@gmail.com>
2019-08-24 19:53:37 -0700

    Lesson 4.5 - Scope - Local scope

--- function-scope.js
+++ function-scope.js
@@ -1,7 +1,12 @@
 // Global scope (convertFahrenheitToCelsius, tempOne, tempTwo)
-    //Local scope (fahrenheit, celsius)
+    // Local scope (fahrenheit, celsius)
+        // Local scope (isFreezing)
 let convertFahrenheitToCelsius = function (fahrenheit) {
     let celsius = (fahrenheit - 32) * 5 / 9
+
+    if (celsius <= 0) {
+        let isFreezing = true
+    }
     return celsius
 }
 
