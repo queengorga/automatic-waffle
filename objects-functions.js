20094ad2cd3ac5e65aecff09caa1df8e6427aa73
Victoria Allen <vickmae.allen@gmail.com>
2019-08-30 22:22:22 -0700

    Lesson 5.3 - Objects with Functions

---
+++ objects-functions.js
@@ -0,0 +1,11 @@
+let myBook = {
+    title: 'A Gentleman in Moscow',
+    author: 'Amor Towles',
+    pageCount: '462'
+}
+
+let myBook = {
+    title: 'A Gentleman in Moscow',
+    author: 'Amor Towles',
+    pageCount: '462'
+}
