fa55615ec237350f792a5653fc2b224f5f547b4c
Victoria Allen <vickmae.allen@gmail.com>
2019-09-07 20:05:45 -0700

    Lesson 5.6 - Methods

--- objects-methods.js
+++ objects-methods.js
@@ -1,7 +1,7 @@
 let restaurant = {
     name: 'Nomnom',
     guestCapacity: 75,
-    guestCount: 0
+    guestCount: 0,
     checkAvailability: function (partySIze) {
         console.log(partySize)
     }
