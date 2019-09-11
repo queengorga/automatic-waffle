2347e5c2bd54cb50819f643376f219e40953f766
Victoria Allen <vickmae.allen@gmail.com>
2019-09-10 23:15:54 -0700

    Lesson 5.6 - Method calls

--- objects-methods.js
+++ objects-methods.js
@@ -1,14 +1,20 @@
 let restaurant = {
     name: 'Nomnom',
     guestCapacity: 75,
-    guestCount: 73,
+    guestCount: 0,
     checkAvailability: function (partySize) {
         let seatsLeft = this.guestCapacity - this.guestCount
         return partySize <= seatsLeft
+    },
+    seatParty: function (partySize) {
+        this.guestCount = this.guestCount + partySize
+    },
+    removeParty: function (partySize) {
+        this.guestCount = this.guestCount - partySize
     }
 }
 
-let status = restaurant.checkAvailability(6)
-
-console.log(status)
-    
+restaurant.seatParty(72)
+console.log(restaurant.checkAvailability(4))
+restaurant.removeParty(5)    
+console.log(restaurant.checkAvailability(4))
