f67a0b520a412fb8401c282ce47ea903d27a1321
Victoria Allen <vickmae.allen@gmail.com>
2019-09-16 23:22:05 -0700

    Lesson 5.8 -Number:methods- math.random,math.floor

--- number-methods.js
+++ number-methods.js
@@ -5,3 +5,9 @@ console.log(num.toFixed(2))
 console.log(Math.round(num))
 console.log(Math.round(num))
 console.log(Math.round(num))
+
+let min = 10
+let max = 20
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+
+console.log(randomNum)
