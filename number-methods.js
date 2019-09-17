1bc8ffd746d3da482b9c0cafe827d341e9878af1
Victoria Allen <vickmae.allen@gmail.com>
2019-09-17 15:29:53 -0700

    Lesson 5.8- Number methods

--- number-methods.js
+++ number-methods.js
@@ -10,4 +10,14 @@ let min = 10
 let max = 20
 let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
 
-console.log(randomNum)
+// Challenge
+// 1-5 - true if correct - false if not correct
+
+let makeGuess = function (guess) {
+    let min = 1
+    let max = 5 
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min 
+    return guess === randomNum
+}
+
+console.log(makeGuess(1))
